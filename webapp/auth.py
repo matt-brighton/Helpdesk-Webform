2b42a93e90b56b4804a02930f916b4263e85bb7e
matt-brighton <mat.brighton@hmrc.gov.uk>
2023-07-20 16:24:07 +0100

    Commit 8

--- webapp/auth.py
+++ webapp/auth.py
@@ -65,7 +65,7 @@ def sign_up_form():
             except:
                 flash("An error occurred, please contact IT if this problem continues")
                 
-            flash('account created!', category='success')
+            flash('Account Created!', category='success')
             login_user(new_user, remember=True)
             return redirect(url_for('views.home'))
         
