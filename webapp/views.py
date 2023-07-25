5e6999ad699a9def1fd5922cc827b7eec49254cd
matt-brighton <mat.brighton@hmrc.gov.uk>
2023-07-25 16:16:31 +0100

    Commit 9

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Users, Roles
 from . import db
@@ -14,6 +14,22 @@ def home():
     return render_template('home.html', user=current_user)
 
 
-@ views.route('/admin')
-def admin_table():
-    return render_template('admin.html', user=current_user)
+
+@ views.route('/admin', methods=['GET'])
+def admin_table():  
+    data = Users.query.order_by(Users.created_date)
+    return render_template('admin.html', user=current_user, data=data)
+
+
+
+@ views.route('/admin', methods=['POST'])
+def update():
+    if request.method == 'POST':
+        data = Users.query.get(request.form.get('idEdit'))
+        
+        data.first_name = request.form['NameEdit']
+        data.email = request.form['EditEmail']
+        
+        db.session.commit()
+        flash("User details updated!")
+        return redirect('/admin')
