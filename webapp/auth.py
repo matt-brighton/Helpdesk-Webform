710641a410c77b3a5565382a72ece27274be0e08
matt-brighton <matthew.brighton@hmrc.gov.uk>
2023-08-01 08:49:19 +0100

    commit 10

--- webapp/auth.py
+++ webapp/auth.py
@@ -7,6 +7,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -25,6 +26,7 @@ def login():
 
     return render_template("login.html", user=current_user)
 
+
 @ auth.route('/logout')
 @ login_required
 def logout():
@@ -35,15 +37,14 @@ def logout():
 @ auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up_form():
 
-    
     if request.method == 'POST':
-    
+
         email = request.form.get('email')
         firstname = request.form.get('firstname')
         password = request.form.get('password')
         passwordconfirm = request.form.get('passwordconfirm')
         user = Users.query.filter_by(email=email).first()
-        
+
         if user:
             flash('email already exists', category='error')
         elif len(email) < 4:
@@ -54,19 +55,54 @@ def sign_up_form():
             flash('passwords do not match', category='error')
         elif len(password) < 7:
             flash('password must be at least 7 characters', category='error')
-            
+
         else:
-            new_user = Users(email = email, first_name = firstname, password = generate_password_hash(password, method='sha256'))
-            
+            new_user = Users(email=email, first_name=firstname,
+                             password=generate_password_hash(password, method='sha256'))
+
             try:
                 db.session.add(new_user)
                 db.session.commit()
-                
+
             except:
                 flash("An error occurred, please contact IT if this problem continues")
-                
+
             flash('Account Created!', category='success')
             login_user(new_user, remember=True)
             return redirect(url_for('views.home'))
-        
-    return render_template("sign_up.html", user = current_user)
+
+    return render_template("sign_up.html", user=current_user)
+
+
+@ auth.route('/new_user', methods=['POST'])
+def new_user():
+    if request.method == 'POST':
+        email = request.form.get('NewEmail')
+        firstname = request.form.get('NewName')
+        password = request.form.get('TempPassword')
+        user = Users.query.filter_by(email=email).first()
+
+        if user:
+            flash('email already exists', category='error')
+        elif len(email) < 4:
+            flash('email must be at least 4 characters', category='error')
+        elif len(firstname) < 2:
+            flash('firstname must be at least 2 characters', category='error')
+        elif len(password) < 7:
+            flash('password must be at least 7 characters', category='error')
+
+        else:
+            new_user = Users(email=email, first_name=firstname,
+                             password=generate_password_hash(password, method='sha256'))
+
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+
+            except:
+                flash("An error occurred, please contact IT if this problem continues")
+
+            flash('Account Created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return redirect(url_for('views.home'))
--- webapp/views.py
+++ webapp/views.py
@@ -14,22 +14,33 @@ def home():
     return render_template('home.html', user=current_user)
 
 
-
 @ views.route('/admin', methods=['GET'])
-def admin_table():  
+def admin_table():
     data = Users.query.order_by(Users.created_date)
     return render_template('admin.html', user=current_user, data=data)
-
 
 
 @ views.route('/admin', methods=['POST'])
 def update():
     if request.method == 'POST':
+
         data = Users.query.get(request.form.get('idEdit'))
-        
+
         data.first_name = request.form['NameEdit']
         data.email = request.form['EditEmail']
-        
+
         db.session.commit()
         flash("User details updated!")
-        return redirect('/admin')
+
+    return redirect('/admin')
+
+
+@ views.route('/delete_user', methods=['POST'])
+def delete_user():
+    if request.method == 'POST':
+
+        data = Users.query.get(request.form.get('idDelete'))
+        db.session.delete(data)
+        db.session.commit()
+
+    return redirect('/')
